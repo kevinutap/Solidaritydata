bb97376efa0f8a47ebeabe7c4b13a9924844b78b
kevinutap <kevin.utap@hioa.no>
2017-09-20 10:56:53 +0200

    Add files via upload

---
+++ js/kevinstart.js
@@ -0,0 +1,13 @@
+/**
+* jPList - jQuery Data Grid Controls 5.2.0.9 - http://jplist.com 
+* Copyright 2016 Miriam Zusin
+*/
+$('document').ready(function(){
+    $('#demo').jplist({				
+	itemsBox: '.list' 
+	,itemPath: '.list-item' 
+	,panelPath: '.jplist-panel'	
+});
+			
+		
+});
